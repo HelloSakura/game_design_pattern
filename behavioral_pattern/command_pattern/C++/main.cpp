ebb9e92c9b70900e0ce7fc81bc879e7611ab3c1a
lucida <Lucida@aladinfun.com>
2024-12-19 12:42:43 +0800

    add makefile

--- behavioral_pattern/command_pattern/C++/main.cpp
+++ behavioral_pattern/command_pattern/C++/main.cpp
@@ -1,2 +1,19 @@
 #include<iostream>
+#include"Command.hpp"
+#include"Invoker.hpp"
+#include"Receiver.hpp"
+#include"CopyCommand.hpp"
 
+
+using namespace std;
+
+int main(void)
+{  
+    Receiver * receiver = new Receiver();
+    Command *cmd = new CopyCommand(receiver, "copy command");
+    Invoker *invoker = new Invoker();
+    invoker->setCommand(cmd);
+    invoker->executeCommand();
+
+    return 0;
+}
